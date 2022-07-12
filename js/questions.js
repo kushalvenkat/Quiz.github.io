aecb0d2f547fad00ed6f52cfa933b0186c6b92a6
kushalvenkat <109140765+kushalvenkat@users.noreply.github.com>
2022-07-12 22:02:40 +0530

    quiz app

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which function among the following lets to register a function to be invoked repeatedly after a certain time?",
+    answer: "setInterval()",
+    options: [
+      "setTimeout()",
+      "setTotaltime()",
+      "setInterval()",
+      "None of the mentioned"
+    ]
+  },
+    {
+    numb: 2,
+    question: "To which object does the location property belong?",
+    answer: "Window",
+    options: [
+      "Window",
+      "Position",
+      "Element",
+      "Location"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the result of the following code snippet? <br> <pre> window.location === document.location </pre>",
+    answer: "True",
+    options: [
+      "False",
+      "True",
+      "1",
+      "0"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a framework?",
+    answer: "JavaScript",
+    options: [
+      "jQuery",
+      ".NET",
+      "JavaScript",
+      "None of the mentioned"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What keyword is used to check whether a given property is valid or not?",
+    answer: "in",
+    options: [
+      "in",
+      "is in",
+      "lies",
+      "exits"
+    ]
+  },
+  {
+    numb: 6,
+    question: 'CSS stands for',
+    answer: 'Cascading style sheets',
+    options: [
+      'Cascade style sheets',
+      'Color and style sheets',
+      'Cascading style sheets',
+      'None of the mentioned'
+    ]
+  },
+  {
+    numb: 7,
+    question: 'How do you write "Hello World" in an alert box?',
+    answer: 'alert("Hello World");',
+    options: [
+      'alert("Hello World");',
+      'msgBox("Hello World");',
+      'prompt("Hello World");',
+      'alertBox("Hello World");'
+    ]
+  },
+  {
+    numb: 8,
+    question: "How do you create a function in JavaScript?",
+    answer: "function myFunction()",
+    options: [
+      "function = myFunction()",
+      'function:myFunction()',
+      "function myFunction()",
+      "None of the mentioned"
+    ]
+  },
+  {
+    numb: 9,
+    question: 'How do you call a function named "myFunction"?',
+    answer: "myFunction()",
+    options: [
+      "myFunction()",
+      "call function myFunction()",
+      "call myFunction()",
+      "None of the mentioned"
+    ]
+  },
+  {
+    numb: 10,
+    question: "HTML stands for ",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Modul Language",
+      "Hyper Text Markup Language",
+      "Hyper Test Markup Language",
+      "Hyperlink Markup Language"
+    ]
+  },
+];
